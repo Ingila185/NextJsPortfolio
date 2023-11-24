87623d117068272a3fa3f1ec0e872ca53beb1516
Ingila Ejaz <Ingila.Ejaz@domestic.hbl.com>
2023-11-24 10:28:42 +0500

    changed color of landing page

--- src/app/components/_NavBar.tsx
+++ src/app/components/_NavBar.tsx
@@ -8,37 +8,40 @@ export default function NavBar()
   const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
 
 return (
- <nav className="flex flex-wrap bg-white border-gray-200 dark:bg-gray-900 ">
-  <div className="max-h-screen items-center justify-between mx-auto">
-  <div className="flex md:order-last space-x-3 md:space-x-0 rtl:space-x-reverse">
-  <button className="rounded-lg px-3 py-2   font-medium hover:bg-slate-100 hover:text-slate-900" onClick={(e) => {setTheme(theme === 'dark' ? 'light' : 'dark')}}>
+ <nav className="flex bg-white border-gray-200 dark:bg-gray-900 ">
+  <div className="items-center justify-between mx-auto">
+  <div className="flex md:order-1 space-x-3 md:space-x-0 rtl:space-x-reverse">
+
+  <button className="rounded-lg px-3 py-2 font-medium hover:bg-slate-100 hover:text-slate-900" onClick={(e) => {setTheme(theme === 'dark' ? 'light' : 'dark')}}>
       { theme === 'light' ? 
         (<svg id="theme-toggle-dark-icon" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg> )
       : (<svg id="theme-toggle-light-icon" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fillRule="evenodd" clipRule="evenodd"></path></svg>)
       }
+      {isNavOpen}
     </button>
-      <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+    
+      <button onClick={(e) => { e.preventDefault(); setIsNavOpen(!isNavOpen); console.log(isNavOpen) }}
+         data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center 
+      p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100
+       focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400
+        dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded={isNavOpen}    
+        >
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
-  </div>
-  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+    </div>
+  <div  className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
     <ul className="flex flex-row font-medium md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50
      md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800
      md:dark:bg-gray-900 dark:border-gray-700">
       <li>
         <a href="/" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent 
-        md:text-blue-700 md:dark:text-blue-500" aria-current="page">
+        md:text-blue-700 md:dark:text-blue-500">
           Home</a>
       </li>
-      <li>
-        <a href="/about" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100
-         md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white
-          dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-          About</a>
-      </li>
+     
       <li>
         <a href="/experience" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 
         md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white 
@@ -81,7 +84,7 @@ return (
           Blog</a>
       </li>
     </ul>
-  </div>
+  </div> 
   </div>
 </nav>
 
--- src/app/components/_experience.tsx
+++ src/app/components/_experience.tsx
@@ -7,7 +7,7 @@ export default function Experience()
 {
     return (
 
-<div className="p-10 grid grid-cols-3 lg: grid-cols-2 md: grid-cols-4 sm: grid-cols-1 ">
+<div className="p-10 grid grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1">
 
 {
 workExperience.map(( exp, idx ) => (
