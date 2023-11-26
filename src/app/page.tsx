27b60c3177f20346682cc59ef574ae6b7e22e16c
Ingila Ejaz <Ingila.Ejaz@domestic.hbl.com>
2023-11-26 13:57:46 +0500

    fixed router from pages to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import NavBar from './components/_NavBar';
 import AboutCard from './components/_aboutCard';
 import Projects from './components/_projects';
 import Certifications from './components/_certifications';
-export default function Home({children}: {children: React.ReactNode}) {
+export default function Home() {
   return (
     <div>
       <IntroCard/>
