292041bc3daa248c70a1cc959477ea96b4979c40
Ingila Ejaz <Ingila.Ejaz@domestic.hbl.com>
2023-11-26 16:30:28 +0500

    Added jumbotron on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-'use client';
-import IntroCard from './components/_introCard';
-import { Children, ReactNode } from 'react';
-import NavBar from './components/_NavBar';
-import AboutCard from './components/_aboutCard';
-import Projects from './components/_projects';
-import Certifications from './components/_certifications';
 import IntroCardJumbotron from './components/_introCardJumbotron';
 export default function Home() {
   return (
