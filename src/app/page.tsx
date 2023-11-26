6c6badf158914b38bd36fc08d525aa3fb8bc268d
Ingila Ejaz <Ingila.Ejaz@domestic.hbl.com>
2023-11-26 16:28:44 +0500

    Added jumbotron on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import NavBar from './components/_NavBar';
 import AboutCard from './components/_aboutCard';
 import Projects from './components/_projects';
 import Certifications from './components/_certifications';
+import IntroCardJumbotron from './components/_introCardJumbotron';
 export default function Home() {
   return (
     <div>
-      <IntroCard/>
+      <IntroCardJumbotron/>
       </div>);
 }
 
